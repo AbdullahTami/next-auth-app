3cced075d9ee370ec8680ef3be6d53ad32e09fb7
abdullahqayid <3boo0odq0v@gmail.com>
2024-09-25 18:28:48 +0300

    extracting token sub id onto session user done

--- auth.ts
+++ auth.ts
@@ -4,6 +4,17 @@ import authConfig from "@/auth.config";
 import db from "@/lib/db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,

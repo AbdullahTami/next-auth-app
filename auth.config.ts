c7e24387420e57ddeed46b0a8f430f5a13d69ebb
abdullahqayid <3boo0odq0v@gmail.com>
2024-09-26 20:30:27 +0300

    integrating Oauth sign in

--- auth.config.ts
+++ auth.config.ts
@@ -1,3 +1,5 @@
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 import bcrypt from "bcryptjs";
 import Credentials from "next-auth/providers/credentials";
@@ -6,6 +8,14 @@ import { getUserByEmail } from "@/data/user";
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       authorize: async (credentials) => {
         const validatedFields = loginSchema.safeParse(credentials);

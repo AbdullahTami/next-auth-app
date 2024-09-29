c388fcca1c14af1b0c59ade5eba41a07f4775bc4
abdullahqayid <3boo0odq0v@gmail.com>
2024-09-29 21:10:09 +0300

    password reset page & form initial setup done

---
+++ app/auth/reset/page.tsx
@@ -0,0 +1,10 @@
+import ResetForm from "@/components/auth/ResetForm";
+import React from "react";
+
+export default function ResetPage() {
+  return (
+    <div>
+      <ResetForm />
+    </div>
+  );
+}
--- components/auth/ResetForm.tsx
+++ components/auth/ResetForm.tsx
@@ -1,32 +1,25 @@
 "use client";
-import React, { useState, useTransition } from "react";
 import CardWrapper from "@/components/auth/CardWrapper";
-import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
 
+import { loginAction } from "@/actions/login";
+import FormError from "@/components/FormError";
+import FormSuccess from "@/components/FormSuccess";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
-  FormMessage,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
-import { loginSchema, LoginValues } from "@/schemas";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import FormError from "@/components/FormError";
-import FormSuccess from "@/components/FormSuccess";
-import { loginAction } from "@/actions/login";
-import { useSearchParams } from "next/navigation";
-import Link from "next/link";
+import { loginSchema, LoginValues } from "@/schemas";
 
-export default function LoginForm() {
-  const searchParams = useSearchParams();
-  const urlError =
-    searchParams.get("error") === "OAuthAccountNotLinked"
-      ? "Email already in use with different provider"
-      : "";
+export default function ResetForm() {
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
@@ -52,10 +45,9 @@ export default function LoginForm() {
 
   return (
     <CardWrapper
-      headerLabel="Welcome back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
-      showSocial
+      headerLabel="Forgot your password?"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
     >
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
@@ -78,37 +70,11 @@ export default function LoginForm() {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="******"
-                      type="password"
-                    />
-                  </FormControl>
-                  <Button
-                    size="sm"
-                    variant="link"
-                    asChild
-                    className="px-0 font-normal"
-                  >
-                    <Link href="/auth/reset">Forgot password?</Link>
-                  </Button>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
           </div>
-          <FormError message={error || urlError} />
+          <FormError message={error} />
           <FormSuccess message={success} />
           <Button type="submit" className="w-full" disabled={isPending}>
-            Login
+            Send reset email
           </Button>
         </form>
       </Form>

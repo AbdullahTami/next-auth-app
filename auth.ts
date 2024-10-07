657d310c21eb0cacae071677a586352545fb89e7
abdullahqayid <3boo0odq0v@gmail.com>
2024-10-07 23:31:53 +0300

    existing session user to account for OAuth logged in users

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import authConfig from "@/auth.config";
 import db from "@/lib/db";
 import { getUserById } from "@/data/user";
 import { getTwoFactorConfirmationByUserId } from "@/data/twoFactorConfirmation";
+import { getAccountByUserId } from "./data/account";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   pages: {
@@ -57,6 +58,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (session.user) {
         session.user.name = token.name;
         session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
       }
 
       return session;
@@ -67,6 +69,9 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       const existingUser = await getUserById(token.sub);
 
       if (!existingUser) return token;
+
+      const existingAccount = await getAccountByUserId(existingUser.id);
+      token.isOAuth = !!existingAccount;
       token.name = existingUser?.name;
       token.email = existingUser?.email;
       token.role = existingUser.role;

5051af2fcce7fda758a9e9eb1dd8f3b67c95cfee
abdullahqayid <3boo0odq0v@gmail.com>
2024-10-08 23:55:55 +0300

    settings schema done

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,8 +1,31 @@
 import * as z from "zod";
 
-export const settingsSchema = z.object({
-  name: z.optional(z.string()),
-});
+export const settingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum(["USER", "ADMIN"]),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(z.string().min(6)),
+  })
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) {
+        return false;
+      }
+
+      if (data.newPassword && !data.password) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: "New password is required!",
+      path: ["newPassword"],
+    }
+  );
+
 export type SettingsValues = z.infer<typeof settingsSchema>;
 
 export const newPasswordSchema = z.object({
